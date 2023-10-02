0d35bcbb214ef10e115ebf419be953e7ca82f915
Dmytro <topdeveloper97@gmail.com>
2023-10-02 06:17:20 +0300

    Add:  meta tag for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@material-ui/core';
 import dynamic from 'next/dynamic';
+import Head from 'next/head.js';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -10,6 +11,13 @@ const ShareProduct = dynamic(() => import('../components/ShareProduct.js'), {
 const IndexPage = () => {
   return (
     <Box>
+      <Head>
+        <title>Social Sharing</title>
+        <meta name="title" content='PaidTabs' />
+        <meta property="og:title" content='PaidTabs Social Sharing' />
+        <meta name="description" content='paid tabs social media sharing' />
+        <meta property="og:description" content='paid tabs social media sharing' />
+      </Head>
       <ToastContainer />
       <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '16px', gap: '12px' }}>
         <Typography variant='h5'>PaidTabs.com Test Project</Typography>
