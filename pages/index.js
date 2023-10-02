3fce1cd0e994c808ada71cd1a1515e1adb8dd6a3
Dmytro <topdeveloper97@gmail.com>
2023-10-02 04:38:37 +0300

    Update: index page style

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
+import { Box, Typography } from '@material-ui/core';
+
 import dynamic from 'next/dynamic';
 const ShareProduct = dynamic(() => import('../components/ShareProduct.js'), {
   ssr: false
 });
 
-export default function IndexPage() {
+const IndexPage = () => {
   return (
-    <div>
-      <h2>PaidTabs.com Test Project</h2>
-
+    <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '16px', gap: '12px' }}>
+      <Typography>PaidTabs.com Test Project</Typography>
       <ShareProduct productURL="https://paidtabs.com/test" />
-    </div>
+    </Box>
   );
 }
+
+export default IndexPage
